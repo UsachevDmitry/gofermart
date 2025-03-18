06b77edcdd26225a9ec50dd583d56f5d71758feb
dmitry.usachev <dmitry.usachev@indeed-id.com>
2025-03-19 00:05:55 +0300

    fix migrations

--- cmd/gophermart/utils/migration.go
+++ cmd/gophermart/utils/migration.go
@@ -4,7 +4,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
-	"github.com/golang-migrate/migrate/v4"
+	//"github.com/golang-migrate/migrate/v4"
 	_ "github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
