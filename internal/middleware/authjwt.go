af8c1be9c56fed8acd15f24ab03791eeacd31108
dmitry.usachev <dmitry.usachev@indeed-id.com>
2025-03-22 13:12:42 +0300

    fix for  autotests

--- internal/middleware/authjwt.go
+++ internal/middleware/authjwt.go
@@ -2,82 +2,57 @@ package middleware
 
 import (
     "net/http"
-    "strings"
+    //"strings"
     "github.com/gin-gonic/gin"
-    "github.com/golang-jwt/jwt/v5"
+    //"github.com/golang-jwt/jwt/v5"
     "utils"
 )
 
 var jwtSecret = []byte("secret-key") // ваш секретный ключ
 
-// AuthMiddleware проверяет JWT-токен
-func AuthMiddleware() gin.HandlerFunc {
-    return func(ctx *gin.Context) {
-        authHeader := ctx.GetHeader("Authorization")
-        if authHeader == "" {
-            ctx.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
-            ctx.Abort()
-            return
-        }
-
-        // Извлечение токена из заголовка
-        tokenString := strings.TrimPrefix(authHeader, "Bearer ")
-        if tokenString == authHeader {
-            ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный формат токена"})
-            ctx.Abort()
-            return
-        }
-
-        // Парсинг и проверка токена
-        token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-            return jwtSecret, nil
-        })
-        if err != nil || !token.Valid {
-            ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный токен"})
-            ctx.Abort()
-            return
-        }
-
-        // Добавление данных из токена в контекст
-        if claims, ok := token.Claims.(jwt.MapClaims); ok {
-            ctx.Set("login", claims["login"])
-        }
-
-        // Получаем токен из cookie
-		token_cookie, err_cookie := ctx.Cookie("token")
-		if err_cookie != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
-			ctx.Abort()
-			return
-		}
-
-		// Проверяем токен
-		login, err := utils.ValidateJWT(token_cookie)
-		if err != nil {
-			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный токен"})
-			ctx.Abort()
-			return
-		}
-
-		// Добавляем логин в контекст
-		ctx.Set("login", login)
-
-        ctx.Next()
-    }
-}
-
+// // AuthMiddleware проверяет JWT-токен
 // func AuthMiddleware() gin.HandlerFunc {
-// 	return func(ctx *gin.Context) {
-// 		// Получаем токен из cookie
-// 		token, err := ctx.Cookie("token")
-// 		if err != nil {
+//     return func(ctx *gin.Context) {
+//         authHeader := ctx.GetHeader("Authorization")
+//         if authHeader == "" {
+//             ctx.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
+//             ctx.Abort()
+//             return
+//         }
+
+//         // Извлечение токена из заголовка
+//         tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+//         if tokenString == authHeader {
+//             ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный формат токена"})
+//             ctx.Abort()
+//             return
+//         }
+
+//         // Парсинг и проверка токена
+//         token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+//             return jwtSecret, nil
+//         })
+//         if err != nil || !token.Valid {
+//             ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный токен"})
+//             ctx.Abort()
+//             return
+//         }
+
+//         // Добавление данных из токена в контекст
+//         if claims, ok := token.Claims.(jwt.MapClaims); ok {
+//             ctx.Set("login", claims["login"])
+//         }
+
+//         // Получаем токен из cookie
+// 		token_cookie, err_cookie := ctx.Cookie("token")
+// 		if err_cookie != nil {
 // 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
 // 			ctx.Abort()
 // 			return
 // 		}
 
 // 		// Проверяем токен
-// 		login, err := utils.ValidateJWT(token)
+// 		login, err := utils.ValidateJWT(token_cookie)
 // 		if err != nil {
 // 			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный токен"})
 // 			ctx.Abort()
@@ -86,6 +61,31 @@ func AuthMiddleware() gin.HandlerFunc {
 
 // 		// Добавляем логин в контекст
 // 		ctx.Set("login", login)
-// 		ctx.Next()
-// 	}
+
+//         ctx.Next()
+//     }
 // }
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		// Получаем токен из cookie
+		token, err := ctx.Cookie("token")
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "токен отсутствует"})
+			ctx.Abort()
+			return
+		}
+
+		// Проверяем токен
+		login, err := utils.ValidateJWT(token)
+		if err != nil {
+			ctx.JSON(http.StatusUnauthorized, gin.H{"error": "неверный токен"})
+			ctx.Abort()
+			return
+		}
+
+		// Добавляем логин в контекст
+		ctx.Set("login", login)
+		ctx.Next()
+	}
+}
