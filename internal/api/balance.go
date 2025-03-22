f06f658d79b64b84e740be20078dda5eaba6bc5e
dmitry.usachev <dmitry.usachev@indeed-id.com>
2025-03-22 13:25:34 +0300

    fix for  autotests

--- internal/api/balance.go
+++ internal/api/balance.go
@@ -14,6 +14,35 @@ type BalanceResponse struct {
     Withdrawn float64 `json:"withdrawn"`
 }
 
+// func (server *Server) getBalance(ctx *gin.Context) {
+//     // Проверка аутентификации
+//     login, exists := ctx.Get("login")
+//     if !exists {
+//         ctx.JSON(http.StatusUnauthorized, gin.H{"error": "пользователь не аутентифицирован"}) // 401
+//         return
+//     }
+// 	user, _ := server.store.GetUser(ctx, login.(string))
+// 	userID := user.ID
+// 	// Преобразование userID в pgtype.Int4
+// 	userIDInt4 := pgtype.Int4{Int32: userID, Valid: true}
+
+//     // Получаем данные о балансе пользователя
+//     balance, err := server.store.GetBalanceByUserID(ctx.Request.Context(), userIDInt4)
+//     if err != nil {
+//         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+//         return
+//     }
+
+//     // Форматируем ответ
+//     response := BalanceResponse{
+//         Current:   balance.CurrentBalance,
+//         Withdrawn: balance.WithdrawnBalance,
+//     }
+
+//     // Возвращаем ответ
+//     ctx.JSON(http.StatusOK, response) // 200
+// }
+
 func (server *Server) getBalance(ctx *gin.Context) {
     // Проверка аутентификации
     login, exists := ctx.Get("login")
@@ -21,10 +50,14 @@ func (server *Server) getBalance(ctx *gin.Context) {
         ctx.JSON(http.StatusUnauthorized, gin.H{"error": "пользователь не аутентифицирован"}) // 401
         return
     }
-	user, _ := server.store.GetUser(ctx, login.(string))
-	userID := user.ID
-	// Преобразование userID в pgtype.Int4
-	userIDInt4 := pgtype.Int4{Int32: userID, Valid: true}
+    user, err := server.store.GetUser(ctx, login.(string))
+    if err != nil {
+        ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+        return
+    }
+
+    // Преобразование userID в pgtype.Int4
+    userIDInt4 := pgtype.Int4{Int32: user.ID, Valid: true}
 
     // Получаем данные о балансе пользователя
     balance, err := server.store.GetBalanceByUserID(ctx.Request.Context(), userIDInt4)
