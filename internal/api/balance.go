383bec167fe1455fb61060cff7a001987908e3b1
dmitry.usachev <dmitry.usachev@indeed-id.com>
2025-03-22 13:45:47 +0300

    fix for  autotests

--- internal/api/balance.go
+++ internal/api/balance.go
@@ -43,6 +43,44 @@ type BalanceResponse struct {
 //     ctx.JSON(http.StatusOK, response) // 200
 // }
 
+// func (server *Server) getBalance(ctx *gin.Context) {
+//     // Проверка аутентификации
+//     login, exists := ctx.Get("login")
+//     if !exists {
+//         ctx.JSON(http.StatusUnauthorized, gin.H{"error": "пользователь не аутентифицирован"}) // 401
+//         return
+//     }
+//     user, err := server.store.GetUser(ctx, login.(string))
+//     if err != nil {
+//         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+//         return
+//     }
+
+//     // Преобразование userID в pgtype.Int4
+//     userIDInt4 := pgtype.Int4{Int32: user.ID, Valid: true}
+
+//     // Получаем данные о балансе пользователя
+//     balance, err := server.store.GetBalanceByUserID(ctx.Request.Context(), userIDInt4)
+//     if err != nil {
+//         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+//         return
+//     }
+
+//     // Форматируем ответ
+//     response := BalanceResponse{
+//         Current:   balance.CurrentBalance,
+//         Withdrawn: balance.WithdrawnBalance,
+//     }
+
+//     // Возвращаем ответ
+//     ctx.JSON(http.StatusOK, response) // 200
+// }
+
+// type WithdrawRequest struct {
+//     Order string  `json:"order"`
+//     Sum   float64 `json:"sum"`
+// }
+
 func (server *Server) getBalance(ctx *gin.Context) {
     // Проверка аутентификации
     login, exists := ctx.Get("login")
@@ -50,6 +88,8 @@ func (server *Server) getBalance(ctx *gin.Context) {
         ctx.JSON(http.StatusUnauthorized, gin.H{"error": "пользователь не аутентифицирован"}) // 401
         return
     }
+
+    // Получаем пользователя
     user, err := server.store.GetUser(ctx, login.(string))
     if err != nil {
         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
@@ -74,11 +114,6 @@ func (server *Server) getBalance(ctx *gin.Context) {
 
     // Возвращаем ответ
     ctx.JSON(http.StatusOK, response) // 200
-}
-
-type WithdrawRequest struct {
-    Order string  `json:"order"`
-    Sum   float64 `json:"sum"`
 }
 
 // func (server *Server) withdrawBalance(ctx *gin.Context) {
