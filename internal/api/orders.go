203eca69074d68578ecc4738d50cd4c88dd52b60
dmitry.usachev <dmitry.usachev@indeed-id.com>
2025-03-22 14:17:00 +0300

    fix for autotests

--- internal/api/orders.go
+++ internal/api/orders.go
@@ -116,6 +116,52 @@ type OrderResponse struct {
 //     ctx.JSON(http.StatusOK, response) // 200
 // }
 
+// func (server *Server) getOrders(ctx *gin.Context) {
+//     // Проверка аутентификации
+//     login, exists := ctx.Get("login")
+//     if !exists {
+//         ctx.JSON(http.StatusUnauthorized, gin.H{"error": "пользователь не аутентифицирован"}) // 401
+//         return
+//     }
+
+//     // Получаем пользователя
+//     user, err := server.store.GetUser(ctx, login.(string))
+//     if err != nil {
+//         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+//         return
+//     }
+
+//     // Преобразование userID в pgtype.Int4
+//     userIDInt4 := pgtype.Int4{Int32: user.ID, Valid: true}
+
+//     // Получаем список заказов пользователя
+//     orders, err := server.store.GetOrdersByUserID(ctx.Request.Context(), userIDInt4)
+//     if err != nil {
+//         ctx.JSON(http.StatusInternalServerError, gin.H{"error": "внутренняя ошибка сервера"}) // 500
+//         return
+//     }
+
+//     // Если заказов нет, возвращаем 204
+//     if len(orders) == 0 {
+//         ctx.Status(http.StatusNoContent) // 204
+//         return
+//     }
+
+//     // Форматируем ответ
+//     var response []OrderResponse
+//     for _, order := range orders {
+//         response = append(response, OrderResponse{
+//             Number:     order.OrderNumber,
+//             Status:     order.Status,
+//             Accrual:    order.Accrual.Float64,
+//             UploadedAt: order.UploadedAt.Time,
+//         })
+//     }
+
+//     // Возвращаем ответ
+//     ctx.JSON(http.StatusOK, response) // 200
+// }
+
 func (server *Server) getOrders(ctx *gin.Context) {
     // Проверка аутентификации
     login, exists := ctx.Get("login")
